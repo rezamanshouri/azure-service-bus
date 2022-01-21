480d45465f06e96a8931c177b997a685af05f456
Reza Manshouri <rmanshouri@microsoft.com>
2022-01-21 10:26:07 -0800

    Update appsettings

--- ServiceBusProducer/Publish/MessagePublisher.cs
+++ ServiceBusProducer/Publish/MessagePublisher.cs
@@ -27,7 +27,7 @@
             // Application properties can be used to add filters at subscription level.
             // To do so, navigate to 'Correlation Filter' -> 'CUSTOM PROPERTIES'
             message.ApplicationProperties["messageType"] = typeof(T).Name;
-            
+
             return serviceBusSender.SendMessageAsync(message);
         }
 
